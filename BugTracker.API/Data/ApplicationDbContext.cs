0ffe2815c04236147cae529b4d86b01c95b35a50
Oyne <andreoyy.litvinovv@gmail.com>
2025-02-25 23:27:27 +0200

    Context Added

---
+++ BugTracker.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using BugTracker.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BugTracker.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { Database.EnsureCreated(); }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Priority> Priorities { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+        public DbSet<Bug> Bugs { get; set; }
+
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<AppUser> Users { get; set; }
+
+        public DbSet<AuthorBug> AuthorsBugs { get; set; }
+        public DbSet<AssigneeBug> AssigneesBugs { get; set; }
+    }
+}
--- BugTracker.API/Program.cs
+++ BugTracker.API/Program.cs
@@ -1,5 +1,6 @@
-
+using BugTracker.API.Data;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 
 namespace BugTracker.API
 {
@@ -9,14 +10,17 @@ namespace BugTracker.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
             // Verify connection to database
             using var connection = new SqlConnection(builder.Configuration.GetConnectionString("DefaultConnection"));
             connection.Open();
             Console.WriteLine("Database connected successfully!");
 
             // Add services to the container.
-
             builder.Services.AddControllers();
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
