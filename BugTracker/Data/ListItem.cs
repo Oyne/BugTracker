7f172fa340343b0078027570b50a8e907bdd417f
Oyne <andreoyy.litvinovv@gmail.com>
2025-02-25 20:17:15 +0200

    Init

---
+++ BugTracker/Data/ListItem.cs
@@ -0,0 +1,10 @@
+﻿namespace BugTracker.Data
+{
+    public class ListItem
+    {
+        public string Title { get; set; } = "Title";
+        public string Description { get; set; } = "Description";
+        public string Color { get; set; } = "gray";
+        public bool IsSelected { get; set; } = false;
+    }
+}
