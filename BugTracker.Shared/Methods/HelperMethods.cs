e513e7dab9e7537ba18ed30bc198efe3e3194fe2
Oyne <andreoyy.litvinovv@gmail.com>
2025-05-02 14:28:39 +0300

    Helper methods

---
+++ BugTracker.Shared/Methods/HelperMethods.cs
@@ -0,0 +1,28 @@
+﻿using BugTracker.Shared.Models;
+
+namespace BugTracker.Shared.Methods
+{
+    public static class HelperMethods
+    {
+        public static Bug CloneBug(Bug bug)
+        {
+            Bug clone = new Bug
+            {
+                Id = bug.Id,
+                Title = bug.Title,
+                Description = bug.Description,
+                PriorityId = bug.PriorityId,
+                Priority = bug.Priority,
+                StatusId = bug.StatusId,
+                Status = bug.Status,
+                CategoryId = bug.CategoryId,
+                Category = bug.Category,
+                CreationDate = bug.CreationDate,
+                LastEditDateTime = bug.LastEditDateTime,
+                LoggedTime = bug.LoggedTime,
+            };
+
+            return clone;
+        }
+    }
+}
