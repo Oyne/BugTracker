a9b322f762921e98fd9843924ebb0e579c242445
Oyne <andreoyy.litvinovv@gmail.com>
2025-05-12 00:26:55 +0300

    Toast messages added

---
+++ BugTracker.UI/Models/ToastMessage.cs
@@ -0,0 +1,17 @@
+﻿using BugTracker.UI.Enums;
+
+namespace BugTracker.UI.Models
+{
+    public class ToastMessage
+    {
+        public Guid Id { get; } = Guid.NewGuid();
+        public string Message { get; }
+        public ToastLevel Level { get; }
+
+        public ToastMessage(string message, ToastLevel level)
+        {
+            Message = message;
+            Level = level;
+        }
+    }
+}
