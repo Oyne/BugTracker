051e9d4aee08acc29b74ddbd59656da552cd5be5
Oyne <andreoyy.litvinovv@gmail.com>
2025-05-12 00:16:13 +0300

    Changes in Bug model

--- SQL Scripts/CREATE TABLE.sql
+++ SQL Scripts/CREATE TABLE.sql
@@ -1,10 +1,9 @@
 USE BugTracker
 GO
 
-DROP TABLE AssigneeBug
+DROP TABLE Bug
 DROP TABLE AppUser
 DROP TABLE Role
-DROP TABLE Bug
 DROP TABLE Status
 DROP TABLE Priority
 DROP TABLE Category
@@ -47,11 +46,11 @@ CREATE TABLE AppUser
 (
     Id INT IDENTITY(1,1) PRIMARY KEY,
     Email NVARCHAR(50) NOT NULL UNIQUE,
-    Username NVARCHAR(50) NOT NULL,
+    UserName NVARCHAR(50) NOT NULL UNIQUE,
     Password NVARCHAR(500) NOT NULL,
     FirstName NVARCHAR(25) NULL DEFAULT NULL,
     LastName NVARCHAR(25) NULL DEFAULT NULL,
-    RoleId INT NULL DEFAULT NULL,  
+    RoleId INT NULL DEFAULT NULL,
     FOREIGN KEY (RoleId) REFERENCES Role (Id) ON DELETE SET NULL
 )
 GO
@@ -64,47 +63,47 @@ CREATE TABLE Bug
     PriorityId INT NULL,
     StatusId INT NULL,
     CategoryId INT NULL,
+    AuthorId INT NULL,
+    LastEditorId INT NULL,
+    AssigneeId INT NULL,
     CreationDateTime DATETIME NOT NULL DEFAULT GETDATE(),
     LastEditDateTime DATETIME NOT NULL DEFAULT GETDATE(),
     LoggedTime TIME NULL,
     FOREIGN KEY (PriorityId) REFERENCES Priority (Id) ON DELETE SET NULL,
     FOREIGN KEY (StatusId) REFERENCES Status (Id) ON DELETE SET NULL,
-    FOREIGN KEY (CategoryId) REFERENCES Category (Id) ON DELETE SET NULL
+    FOREIGN KEY (CategoryId) REFERENCES Category (Id) ON DELETE SET NULL,
+    FOREIGN KEY (AuthorId) REFERENCES AppUser (Id) ON DELETE NO ACTION,
+    FOREIGN KEY (LastEditorId) REFERENCES AppUser (Id) ON DELETE SET NULL,
+    FOREIGN KEY (AssigneeId) REFERENCES AppUser (Id) ON DELETE NO ACTION
 )
 GO
 
-CREATE TABLE AssigneeBug
-(
-    Id INT IDENTITY(1,1) PRIMARY KEY,
-    AssigneeId INT NOT NULL,
-    BugId INT NOT NULL,
-    FOREIGN KEY (AssigneeId) REFERENCES AppUser (Id) ON DELETE CASCADE,
-    FOREIGN KEY (BugId) REFERENCES Bug (Id) ON DELETE CASCADE
-)
+INSERT INTO Status
+    (Name, Color)
+VALUES
+    ('Todo', '#1f873a'),
+    ('In Progress', '#9b6d17'),
+    ('Done', '#904de2')
 GO
 
-INSERT INTO Status (Name, Color)
-VALUES 
-('Todo', '#1f873a'),
-('In Progress', '#9b6d17'),
-('Done', '#904de2')
+INSERT INTO Priority
+    (Name, Color)
+VALUES
+    ('Low', '#1f873a'),
+    ('Medium', '#9b6d17'),
+    ('High', '#eb3645')
 GO
 
-INSERT INTO Priority (Name, Color)
-VALUES 
-('Low', '#1f873a'),
-('Medium', '#9b6d17'),
-('High', '#eb3645')
+INSERT INTO Category
+    (Name, Color)
+VALUES
+    ('UI', '#1f873a'),
+    ('API', '#9b6d17')
 GO
 
-INSERT INTO Category (Name, Color)
-VALUES 
-('UI', '#1f873a'),
-('API', '#9b6d17')
-GO
-
-INSERT INTO Role (Name, Color)
-VALUES 
-('Admin', '#1f873a'),
-('User', '#9b6d17')
+INSERT INTO Role
+    (Name, Color)
+VALUES
+    ('Admin', '#1f873a'),
+    ('User', '#9b6d17')
 GO
