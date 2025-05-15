a6b1f4bde86d2038d0338526a380d2f8d7dc0d4a
Oyne <andreoyy.litvinovv@gmail.com>
2025-05-15 16:59:17 +0300

    Created classes with messages/warnings Changed auto dismiss delay from 3 to 5 sec for toast messages

---
+++ BugTracker.UI/Services/Messages/BugToastMessages.cs
@@ -0,0 +1,50 @@
+﻿using BugTracker.UI.Enums;
+using BugTracker.UI.Models;
+
+namespace BugTracker.UI.Services.Messages
+{
+    public static class BugToastMessages
+    {
+        private static readonly string _entityName = "Bug";
+        private static readonly string _parameter = "Title";
+
+        public static ToastMessage BugCreated(string title = "")
+        {
+            return string.IsNullOrEmpty(title) ?
+                GeneralToastMessages.Created(_entityName) :
+                new ToastMessage(
+                    message: $"{_entityName} \"{title}\" created.",
+                    level: ToastLevel.Success);
+        }
+
+        public static ToastMessage BugCreationError =>
+            GeneralToastMessages.CreationError(_entityName);
+
+        public static ToastMessage BugWithTitleAlreadyExists(string title) =>
+            GeneralToastMessages.EntityWithParameterAlreadyExists(
+                entityName: _entityName,
+                parameter: _parameter,
+                value: title);
+
+        public static ToastMessage BugUpdated =>
+            GeneralToastMessages.Updated(_entityName);
+
+        public static ToastMessage BugUpdateError =>
+            GeneralToastMessages.UpdateError(_entityName);
+
+        public static ToastMessage BugDeleted(string title = "")
+        {
+            return string.IsNullOrEmpty(title) ?
+                GeneralToastMessages.Deleted(_entityName) :
+                new ToastMessage(
+                    message: $"{_entityName} \"{title}\" deleted.",
+                    level: ToastLevel.Success);
+        }
+
+        public static ToastMessage BugDeletionError =>
+            GeneralToastMessages.DeletionError(_entityName);
+
+        public static ToastMessage BugNotFound =>
+            GeneralToastMessages.NotFound(_entityName);
+    }
+}
---
+++ BugTracker.UI/Services/Messages/UserToastMessages.cs
@@ -0,0 +1,66 @@
+﻿using BugTracker.UI.Enums;
+using BugTracker.UI.Models;
+using BugTracker.UI.Services.Warnings;
+
+namespace BugTracker.UI.Services.Messages
+{
+    public class UserToastMessages
+    {
+        private static readonly string _entityName = "User";
+        private static readonly string _emailParameter = "email";
+        private static readonly string _userNameParameter = "username";
+
+        public static ToastMessage UserRegistered(string userName) =>
+                new ToastMessage(
+                    message: $"{_entityName} \"{userName}\" registered.",
+                    level: ToastLevel.Success);
+
+        public static ToastMessage UserLoggedIn(string userName) =>
+                new ToastMessage(
+                    message: $"Welcome \"{userName}\".",
+                    level: ToastLevel.Success);
+
+        public static ToastMessage InvalidLoginData =>
+       new ToastMessage(
+           message: GeneralWarnings.InvalidLoginData,
+           level: ToastLevel.Error);
+
+        public static ToastMessage UserRegistrationError =>
+            new ToastMessage(
+                message: $"There was an error registering the {_entityName.ToLower()}.",
+                level: ToastLevel.Error);
+
+        public static ToastMessage UserWithEmailAlreadyExists(string email) =>
+            GeneralToastMessages.EntityWithParameterAlreadyExists(
+                entityName: _entityName,
+                parameter: _emailParameter,
+                value: email);
+
+        public static ToastMessage UserWithUserNameAlreadyExists(string userName) =>
+            GeneralToastMessages.EntityWithParameterAlreadyExists(
+                entityName: _entityName,
+                parameter: _userNameParameter,
+                value: userName);
+
+        public static ToastMessage UserUpdated =>
+            GeneralToastMessages.Updated(_entityName);
+
+        public static ToastMessage BugUpdateError =>
+            GeneralToastMessages.UpdateError(_entityName);
+
+        public static ToastMessage UserDeleted(string userName = "")
+        {
+            return string.IsNullOrEmpty(userName) ?
+                GeneralToastMessages.Deleted(_entityName) :
+                new ToastMessage(
+                    message: $"{_entityName} \"{userName}\" deleted.",
+                    level: ToastLevel.Success);
+        }
+
+        public static ToastMessage UserDeletionError =>
+            GeneralToastMessages.DeletionError(_entityName);
+
+        public static ToastMessage UserNotFound =>
+            GeneralToastMessages.NotFound(_entityName);
+    }
+}
